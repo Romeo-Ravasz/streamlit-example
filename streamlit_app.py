dc03748946ef0aa78e8173ecd3a14c4e78757fbf
Romeo-Ravasz <90318651+Romeo-Ravasz@users.noreply.github.com>
2023-11-29 14:57:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,20 +6,20 @@ st.write("---")
 num1 = st.number_input(label = "Első szám:")
 num2 = st.number_input(label = "Második szám:")
 muvelet = st.radio("Műveletek", ("összeadás", "kivonás", "szorzás", "osztás"))
-vegeredmeny = 0
+ans = 0
 def kiszámolás():
     if muvelet == "összeadás":
-        vegeredmeny == num1 + num2
+        ans == num1 + num2
     elif muvelet == "kivonás":
-        vegeredmeny == num1 - num2
+        ans == num1 - num2
     elif muvelet == "szorzás":
-        vegeredmeny == num1 * num2
+        ans == num1 * num2
     elif muvelet == "osztás" and num2!=0:
-        vegeredmény == num1 / num2
+        ans == num1 / num2
     else:
         st.warning("Valami elromlott :/")
 
-    st.success(f"A végeredmény: {vegeredmeny}")
+    st.success(f"A végeredmény: {ans}")
 fasz = st.button("Kalkulálj!")
 if fasz:
     kiszámolás()
