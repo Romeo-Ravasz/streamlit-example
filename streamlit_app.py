87f08e4d2831545bfc93cfc09a0697a07af99b13
Romeo-Ravasz <90318651+Romeo-Ravasz@users.noreply.github.com>
2023-11-29 14:23:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,35 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+st.title("Számológép")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+szam1 = st.number_input(label = szam1)
+szam2 = st.number_input(label = szam2)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.write("geci")
+muvelet = st.radio("Műveletek", ("összeadás", "kivonás", "szorzás", "osztás"))
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+vegeredmeny = 0
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+def kiszámolás():
+    if muvelet = "összeadás":
+        vegeredmeny = szam1 + szam2
+    if muvelet = "kivonás":
+        vegeredmeny = szam1 - szam2
+    if muvelet = "szorzás":
+        vegeredmeny = szam1 * szam2
+    if muvelet = "osztás":
+        vegeredmény = szam1 / szam2
+    else:
+        st.warning("Valami elromlott :/")
+        vegeredmeny = "idk"
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+    st.success(f"A végeredmény: {vegeredmeny})
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+if st.button("Kalkulálj!"):
+    kiszámolás()
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+
+        
+        
+
+
