2fefea1ac7df052eb9a384938a95a59fc4f0368f
Romeo-Ravasz <90318651+Romeo-Ravasz@users.noreply.github.com>
2023-12-07 08:24:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,12 @@
 import streamlit as st
 
-st.title("Számológép")
+st.title("Regisztráció")
 st.write("---")
 
-num1 = st.number_input(label = "Első szám:")
-num2 = st.number_input(label = "Második szám:")
-muvelet = st.radio("Műveletek", ("összeadás", "kivonás", "szorzás", "osztás"))
-ans = 0
-def kiszámolás():
-    if muvelet == "összeadás":
-        ans == num1 + num2
-    elif muvelet == "kivonás":
-        ans == num1 - num2
-    elif muvelet == "szorzás":
-        ans == num1 * num2
-    elif muvelet == "osztás" and num2!=0:
-        ans == num1 / num2
-    else:
-        st.warning("Valami elromlott :/")
-
-    st.success(f"A végeredmény: {ans}")
-fasz = st.button("Kalkulálj!")
-if fasz:
-    kiszámolás()
-
+email = st.input(label = "Add meg az email címed:")
+nev = st.input(label = "Add meg a neved:")
+pencil = st.radio("Add meg a nemed:", ("férfi", "Nő"))
+eletkor = st.slider("Add meg az életkorodat:", (0, 100))
 
         
         
